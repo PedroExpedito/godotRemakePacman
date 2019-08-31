462f74ef7420a86bac392182c7611eed0c619c41
PedroExpedito <expeditocaf00@gmail.com>
2019-08-31 18:14:15 -0300

    video1

---
+++ godot/Pacman-Ramake/src/scripts/pacman_player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+var motion = Vector2()
+var speed = 100000
+var move = true
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_down") and move == false:
+		motion.y = speed*delta
+		move = true
+	elif Input.is_action_pressed("ui_up") and move ==false:
+		motion.y = -speed*delta
+		move = true
+	elif Input.is_action_pressed("ui_right") and move ==false:
+		motion.x = speed*delta
+		move = true
+	elif Input.is_action_pressed("ui_left") and move ==false:
+		motion.x = -speed*delta	
+		move = true
+	else:
+		move = false
+		motion = Vector2()	
+			
+		
+	motion = move_and_slide(motion)
